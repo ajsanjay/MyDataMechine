5377d0e0d892d59343c704d876dbd2a8f406cf11
Jaya Sabeen <jayasabeen@Jayas-MacBook-Pro.local>
2024-04-30 02:01:36 +0530

    Alert content changes

--- MyDataMechine/Utilities/Alert.swift
+++ MyDataMechine/Utilities/Alert.swift
@@ -16,6 +16,6 @@ struct AlertItem: Identifiable {
 
 struct AlertContext {
     static let PageCountEmpty = AlertItem(title: Text("Page Count Empty"), message: Text("Please input a page count to proceed"), dismissButton: .default(Text("Ok")))
-    static let PageCountGreaterthan1 = AlertItem(title: Text("Cannot proceed this count"), message: Text("Please input a count greater than 1"), dismissButton: .default(Text("Ok")))
+    static let PageCountGreaterthan1 = AlertItem(title: Text("Cannot proceed this count"), message: Text("Please input a count greater or equal to 1"), dismissButton: .default(Text("Ok")))
     static let PageCountGreatergreaterThanTotal = AlertItem(title: Text("Cannot proceed this count"), message: Text("The value enterd is grater than total records so partially loading all details"), dismissButton: .default(Text("Ok")))
 }
