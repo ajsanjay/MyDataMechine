080fec63561202f12af14c76972ccb671f179fad
Jaya Sabeen <jayasabeen@Jayas-MacBook-Pro.local>
2024-04-29 19:31:13 +0530

    Project created

---
+++ MyDataMechine/MyDataMechineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDataMechineApp.swift
+//  MyDataMechine
+//
+//  Created by Jaya Sabeen on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MyDataMechineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
